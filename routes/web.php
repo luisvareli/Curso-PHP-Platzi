221d5e019bc985cfe6f90b2c92de6d1f7891ccdb
Luis Vargas Elizondo <lewiis29@gmail.com>
2020-12-23 16:00:59 -0600

    [Capitulo 21] Trabajando con relaciones

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExpenseController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
@@ -21,5 +22,7 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/dashboard', [DashboardController::class, 'index']);
 
 Route::resource('expense_reports', ExpenseReportController::class);
-
 Route::get('/expense_reports/{id}/confirmDelete', [ExpenseReportController::class, 'confirmDelete']);
+
+Route::get('/expense_reports/{expense_report}/expenses/create', [ExpenseController::class, 'create']);
+Route::post('/expense_reports/{expense_report}/expenses', [ExpenseController::class, 'store']);
